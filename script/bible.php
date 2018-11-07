5fe0d644b5ab2c2defc8fd9d8caa62585ea757ae
Eliran Wong <hkwebtech@gmail.com>
2018-11-07 21:49:57 +0000

    Add files via upload

---
+++ script/bible.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="icon" href="/marvel.png">
+
+<title>Marvel Bible: Marvellous Bible Resources</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="for devotion, study or study or research.">
+<meta name="author" content="Eliran Wong">
+<meta name="keywords" content="marvel,marvellous,bible,app,interlinear,morphology,strong,original,Hebrew,Greek,BHS,GNT,OpenGNT,NA27,NA28,biblebento,lexicon,linguistic,etcbc,annotation,discourse,analysis">
+
+<link rel="stylesheet" type="text/css" href="bible.css?v=0.1">
+
+<script>
+//var curPos;
+var lastLookupWid = 'w0';
+var mod = 'bible';
+var b = <?php echo $_GET["b"]; ?>;
+var c = <?php echo $_GET["c"]; ?>;
+var v = <?php echo $_GET["v"]; ?>;
+</script>
+
+<script src="/marvel.js"></script>
+<script src="/w3.js"></script>
+
+</head>
+
+<body>
+<div onclick="return ''">
+
+<w id='w0'></w>
+
+<?php include '../chapters/' . $_GET["b"] . '_' . $_GET["c"] . '.html';?>
+<hr>
+
+<?php include $_GET["b"] . '_' . $_GET["c"] . '.html';?>
+
+<hr>
+<?php include '../chapters/' . $_GET["b"] . '_' . $_GET["c"] . '.html';?>
+
+<p>&nbsp;</p>
+<div id="footer"></div>
+<script>loadBible(b,c,v)</script>
+
+</div>
+</body>
+
+</html>
---
+++ script/convertBibleBentoPlusToMarvelBible.py
@@ -0,0 +1,85 @@
+import re
+
+inputFile = 'file.json'
+outputFile = 'file.csv'
+
+# open file
+
+f = open(inputFile,'r')
+newData = f.read()
+f.close()
+
+# clear unwanted spaces
+newData = re.sub(' [ ]+?([^ ])', r' \1', newData, flags=re.M)
+newData = re.sub('^ ', r'', newData, flags=re.M)
+newData = re.sub(' $', r'', newData, flags=re.M)
+
+# convert json to tsv
+newData = re.sub('}\n\]', r'},\n-|\n', newData, flags=re.M)
+
+# convert json to tsv
+newData = re.sub('{\n"bNo": ([0-9]+?),\n"cNo": ([0-9]+?),\n"vText": "(.*?)",\n"vNo": ([0-9]+?)\n},', r'\1\t\2\t\4\t\3', newData, flags=re.M)
+newData = re.sub('^0\t0\t0\t.*?\n', r'', newData, flags=re.M)
+
+# separate chapters
+newData = re.sub('^([0-9]+?)\t([0-9]+?)\t1\t', r'-|\n\1\t\2\t1\t', newData, flags=re.M)
+newData = re.sub('\[\n-\|', r'-|', newData, flags=re.M)
+
+# format verse number
+newData = re.sub('^([0-9]+?)\t([0-9]+?)\t([0-9]+?)\t', r'<vid id="v\1.\2.\3" onclick="luV(\3)">\3</vid>', newData, flags=re.M)
+
+# format paragraph
+newData = re.sub('( ¶|¶ )', r'¶', newData, flags=re.M)
+newData = re.sub('( §|§ )', r'§', newData, flags=re.M)
+newData = re.sub('( †|† )', r'†', newData, flags=re.M)
+newData = re.sub('( ‡|‡ )', r'‡', newData, flags=re.M)
+newData = re.sub(' [ ]+?([^ ])', r' \1', newData, flags=re.M)
+newData = re.sub('¶[¶]+?([^¶])', r'¶\1', newData, flags=re.M)
+newData = re.sub('§[§]+?([^§])', r'§\1', newData, flags=re.M)
+newData = re.sub('†[†]+?([^†])', r'†\1', newData, flags=re.M)
+newData = re.sub('‡[‡]+?([^‡])', r'‡\1', newData, flags=re.M)
+newData = re.sub('(§¶|¶§)', r'¶', newData, flags=re.M)
+
+p = re.compile('(<vid.*?</vid>)(【.*?】|¶|§|†|‡)', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'\2\1', newData)
+    s = p.search(newData)
+
+newData = re.sub('</vid>', r'</vid> ', newData, flags=re.M)
+newData = re.sub('¶', r'<br><br>', newData, flags=re.M)
+newData = re.sub('§', r'<br>', newData, flags=re.M)
+newData = re.sub('†', r'<br><br>&emsp;&emsp;', newData, flags=re.M)
+newData = re.sub('‡', r'<br>&emsp;&emsp;', newData, flags=re.M)
+
+newData = re.sub('【(.*?)】', r'<br><br><u><b>\1</b></u>', newData, flags=re.M)
+newData = re.sub('^-\|\n<br><br>', r'-|\n', newData, flags=re.M)
+newData = re.sub('</b></u><br><br><br><u><b>', r'</b></u><br><u><b>', newData, flags=re.M)
+newData = re.sub('</b></u><br><br><u><b>', r'</b></u><br><u><b>', newData, flags=re.M)
+
+# removed unwanted linebreaks
+newData = re.sub('( <br>|<br> )', r'<br>', newData, flags=re.M)
+
+p = re.compile('<br><br><br>', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'<br><br>', newData)
+    s = p.search(newData)
+
+p = re.compile('<br>\n<br><br>', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'\n<br><br>', newData)
+    s = p.search(newData)
+
+p = re.compile('<br><br>\n<br>', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'<br><br>\n', newData)
+    s = p.search(newData)
+
+# close file
+
+f = open(outputFile,'w')
+f.write(newData)
+f.close()
