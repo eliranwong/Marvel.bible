f870824a12d45486873e1017822ab7a7230e4cc8
Eliran Wong <hkwebtech@gmail.com>
2018-12-03 07:13:08 +0000

    Add files via upload

---
+++ script/importMultipleImagesIntoSQLite.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# a script adapted from https://gist.github.com/zed/1304181
+# task: import multiple images into a sqlite file
+import glob
+import os
+import sqlite3
+
+# support both Python 2.x and 3.x
+try: buffer = buffer
+except NameError:
+    buffer = lambda x: x # on Python 3.x 'rb' mode already returns what we need
+
+def U(literal_string):
+    if hasattr(literal_string, 'decode'):
+        return literal_string.decode('utf-8') # source code encoding
+    return literal_string
+
+# open db
+conn = sqlite3.connect('images.sqlite') # specify a database filename here
+conn.execute('''create table if not exists BDB (
+                    path unique not null, 
+                    image blob
+                )''') # specify a table name here
+
+def genimages():
+    """Generate example BDB.""" # specify a table name here
+    for imagepath in glob.iglob(os.path.expanduser(U('BDB/*.gif'))): # specify a path here
+        with open(imagepath, 'rb') as f:
+            yield imagepath, buffer(f.read())
+
+# insert images
+with conn: # insert all or nothing
+    conn.executemany('insert into BDB(path,image) values(?, ?)', genimages()) # specify a table name here
+
+# print image paths
+for path, in conn.execute('select path from BDB'): # specify a table name here
+    print(path)
+
