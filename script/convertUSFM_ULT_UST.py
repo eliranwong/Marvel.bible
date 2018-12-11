aaa0ade596aa6214b516e1472d1002d0b66724e7
Eliran Wong <hkwebtech@gmail.com>
2018-12-11 16:04:04 +0000

    Add files via upload

--- script/convertUSFM_ULT_UST.py
+++ script/convertUSFM_ULT_UST.py
@@ -162,6 +162,31 @@ while s:
     newData = p.sub(r'\n\2\1', newData)
     s = p.search(newData)
 
+# formatting for import into sqlite
+
+# clear unwanted spaces
+
+newData = re.sub('\n[\n]+?([^\n])', r'\n\1', newData, flags=re.M)
+
+newData = re.sub('\A\n', r'', newData, flags=re.M)
+
+newData = re.sub('^(.*?<vid id="v)([0-9]+?)\.([0-9]+?)\.', r'\2\t\3\t\1\2.\3.', newData, flags=re.M)
+
+p = re.compile(r'^([0-9]+?\t)0\t(.*?\n)\1([0-9]+?\t)', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'\1\3\2\1\3', newData)
+    s = p.search(newData)
+
+p = re.compile(r'^([0-9]+?\t[0-9]+?\t)(.*?)\n\1', flags=re.M)
+s = p.search(newData)
+while s:
+    newData = p.sub(r'\1\2 ', newData)
+    s = p.search(newData)
+
+# remove chapter divider
+newData = re.sub('^\-\|\n', r'', newData, flags=re.M)
+
 # close file
 f = open(outputFile,'w')
 f.write(newData)
